97a4b62d8fba17c4de14c559e693e15b50fd85c5
Beadie1023 <44064754+Beadie1023@users.noreply.github.com>
2026-06-04 17:47:28 -0400

    Remove unused plugins from Vite config
    Removed unused plugins: react and tailwindcss.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { tailwindcss } from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
@@ -10,8 +8,6 @@ export default defineConfig({
       routesDirectory: "./src/routes",
       generatedRouteTree: "./src/routeTree.gen.ts",
     }),
-    react(),
-    tailwindcss(),
     tsconfigPaths(),
   ],
 });
